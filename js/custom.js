1cb4790801c6dbe4a9773d404f2e98985d295620
joe <freddygekko3@gmail.com>
2021-11-01 20:45:48 +0200

    page

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+
+
+// PRELOADER
+
+$(window).load(function(){
+    $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
+});
+
+
+$(function(){
+    jQuery(document).ready(function() {
+		$('body').backstretch([
+	 		  
+			  "images/bg (2).jpg", 
+			  "images/bg (3).jpg", 
+			  "images/bg (1).jpg", 
+			  "images/bg (4).jpg",
+			  "images/bg (6).jpg", 
+			  "images/bg (7).jpg", 
+
+	 			], 	{duration: 3000, fade: 1500});
+		});
+})
---
+++ test.py
@@ -0,0 +1,12 @@
+import time
+
+def main():
+        {
+            for i in range(10):
+            print(i)
+            if (i / 5) == int :
+                print("found one")
+
+            john = 45 + 40 % 9 
+            #How are you doin
+        }    
